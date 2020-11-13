b029d5b5358b4f12c3fcc349d79375289a38aab5
Samad Ali Shah <samadali1697@gmail.com>
2020-11-13 23:35:02 +0100

    login page added (#1)
    Co-authored-by: Samad Ali Shah <samadali@Samads-MBP.local>

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/js/Home";
 import NotFound from "./containers/js/NotFound";
+import Login from "./containers/js/Login";
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+
+      <Route exact path="/login">
+        <Login />
       </Route>
 
       {/* Finally, catch all unmatched routes */}
---
+++ src/containers/js/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/Login.css";
+
+export default function Login() {
+  return (
+    <div className="Login">
+      <div className="lander">
+        <h1>I am Login Page :)</h1>
+        <p className="text-muted">A simple note taking app</p>
+      </div>
+    </div>
+  );
+}
