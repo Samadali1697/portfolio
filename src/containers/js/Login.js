c7f8b8419af269f2e5c4c72576cf4e8f2620aa6a
Samad Ali Shah <samadali@Samads-MBP.local>
2020-11-14 18:26:48 +0100

    login form added

--- src/containers/js/Login.js
+++ src/containers/js/Login.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/Login.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert("Logged In");
+  }
+
   return (
     <div className="Login">
-      <div className="lander">
-        <h1>I am Login Page :)</h1>
-        <p className="text-muted">A simple note taking app</p>
-      </div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+          Login
+        </Button>
+      </Form>
     </div>
   );
 }
